8517f9f735f19f9e8f92ca0c539e0ec0a7787462
iliya habibi <ihabibi1@bcit.ca>
2024-04-12 03:36:43 -0700

    class Phone and phone.sql added.

---
+++ src/model/da/phone.sql
@@ -0,0 +1,14 @@
+CREATE TABLE PHONE
+(
+    ID            INT PRIMARY KEY,
+    first_name    NVARCHAR2(60),
+    last_name     NVARCHAR2(60),
+    brand         NVARCHAR2(80),
+    price         DECIMAL(10, 2),
+    color         NVARCHAR2(60),
+    serial_number NVARCHAR2(20)
+);
+
+CREATE SEQUENCE PERSON_SEQ START WITH 1 INCREMENT BY 1;
+
+commit;
