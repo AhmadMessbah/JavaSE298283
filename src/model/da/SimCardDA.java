1aff711f6b53c397490da0d06f6d747f975c881d
mobinamirzaee <mobinayoun14@gmail.com>
2024-04-12 14:44:34 +0330

    simcard da added

--- src/model/da/SimCardDA.java
+++ src/model/da/SimCardDA.java
@@ -23,7 +23,6 @@ public class SimCardDA {
     }
 
 
-
     public void save(SimCard simcard) throws Exception {
         connect();
         PreparedStatement preparedStatement = connection.prepareStatement(
@@ -36,19 +35,4 @@ public class SimCardDA {
         disconnect();
     }
 
-    public void edit(SimCard simcard) throws Exception {
-        Connection connection = DriverManager.getConnection(
-                "jdbc:oracle:thin:@localhost:1521:xe",
-                "javase",
-                "java123"
-        );
-        PreparedStatement preparedStatement = connection.prepareStatement(
-                "INSERT INTO SIM_CARD(ID,SIM_NUMBER,PUK_NUMBER,OPERATOR) VALUES (SIM_CARD_SEQ.NEXTVAL , ?,?,?)"
-        );
-        preparedStatement.setString(1, simcard.getSimNumber());
-        preparedStatement.setString(2, simcard.getPukNumber());
-        preparedStatement.setString(3, simcard.getOperator());
-        preparedStatement.execute();
-        preparedStatement.close();
-        connection.close();
-    }
+
