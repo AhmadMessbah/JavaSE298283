b3ce41ef1abcd7df73703b7c553f47645a9f4588
MhtbSd <mhtb.sd82@gmail.com>
2024-04-12 13:33:13 +0330

    license_diagram added

---
+++ src/model/entity/License.java
@@ -0,0 +1,26 @@
+package model.entity;
+
+import com.google.gson.Gson;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
+
+public class License {
+    private int id;
+    private String licenseType;
+    private String fileName;
+    private int fileSize;
+    private String description;
+
+    @Override
+    public String toString() {
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
+}
